e2028cbb8a5b1713923c400bc4682ad7066f3ab2
Lúcás Críostóir Meier <cronokirby@gmail.com>
2022-03-04 22:32:23 +0100

    Setup trivial Rust FFI

---
+++ rust-bits/src/lib.rs
@@ -0,0 +1,4 @@
+#[no_mangle]
+pub extern "C" fn double(x: i32) -> i32 {
+    2 * x
+}
--- src/Main.hs
+++ src/Main.hs
@@ -2,5 +2,9 @@ module Main where
 
 import Ourlude
 
+import Foreign.C.Types
+
+foreign import ccall "double" double :: CInt -> CInt
+
 main :: IO ()
-main = "Hello, Haskell!" |> putStrLn
+main = 3 |> double |> print
