83a1e24b17d38c55c36bc478d8d83e4bc5359473
Lúcás Críostóir Meier <cronokirby@gmail.com>
2022-03-04 23:16:30 +0100

    Setup example of Bytestring in FFI

--- rust-bits/src/lib.rs
+++ rust-bits/src/lib.rs
@@ -2,3 +2,10 @@
 pub extern "C" fn double(x: i32) -> i32 {
     2 * x
 }
+
+const S: &'static [u8] = b"hello";
+
+#[no_mangle]
+pub extern "C" fn init(out: *mut u8) {
+    unsafe { out.copy_from(S.as_ptr(), S.len()) }
+}
--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,16 @@ module Main where
 
 import Ourlude
 
+import Data.ByteString
+import Data.ByteString.Internal
+import Data.Word
 import Foreign.C.Types
+import Foreign.Ptr
 
-foreign import ccall "double" double :: CInt -> CInt
+foreign import ccall unsafe "init" c_init :: Ptr Word8 -> IO ()
+
+initBS :: ByteString
+initBS = unsafeCreate 5 c_init
 
 main :: IO ()
-main = 3 |> double |> print
+main = initBS |> print
