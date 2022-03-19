d7cdc7ec7f0a06beee67710b7b269c2531cac9f5
Lúcás Críostóir Meier <cronokirby@gmail.com>
2022-03-19 12:05:37 +0100

    Add tests for signature generation in Haskell

--- test/Test.hs
+++ test/Test.hs
@@ -1,6 +1,32 @@
-module Main where 
+{-# LANGUAGE OverloadedStrings #-}
 
+module Main where
+
+import qualified Crypto.Signature as Sig
 import Ourlude
+import Test.Tasty
+import Test.Tasty.HUnit
 
 main :: IO ()
-main = putStrLn "tests"
+main = defaultMain allTests
+
+allTests :: TestTree
+allTests = testGroup "Tests" [unitTests]
+
+unitTests :: TestTree
+unitTests = testGroup "Unit" [signatureTests]
+
+signatureTests :: TestTree
+signatureTests =
+  testGroup
+    "Crypto.Signature"
+    [ testSignatureVerification
+    ]
+
+testSignatureVerification =
+  testCase "testSignatureVerification" <| do
+    priv <- Sig.generatePrivateKey
+    let pub = Sig.privateToPublic priv
+        msg = "msg"
+        sig = Sig.sign priv msg
+    Sig.verify pub msg sig @=? True
